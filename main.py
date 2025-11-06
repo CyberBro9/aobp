2d376767b50544aa3957836a94c9b8b445777e4b
CyberBro9 <levboxx@outlook.com>
2025-11-06 19:51:48 +0500

    Update imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import xlrd
-from PyQt6 import *
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 def parseDocument(path):
     """
@@ -9,3 +9,4 @@ def parseDocument(path):
     :param path: Path to the Excel file.
     """
     return xlrd.open_workbook(path)
+
