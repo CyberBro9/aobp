a968b5ac706901e566bcf644595b018547c5605d
CyberBro9 <levboxx@outlook.com>
2025-11-12 17:15:33 +0500

    Couldn't be bothered to make a commit message.

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import xlrd
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtGui import QAction
 
+# Source - https://stackoverflow.com/a
+# Posted by tomvodi, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-11-12, License - CC BY-SA 4.0
+
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
 from mainUI import Ui_MainWindow
 
 def parseDocument(path):
@@ -16,13 +26,24 @@ def parseDocument(path):
 
 
 class Application(QMainWindow):
+    """
+    Useless OOP declaration of the Qt application.
+    """
     def __init__(self):
         super(Application, self).__init__()
         self.UI = Ui_MainWindow()
         self.UI.setupUi(self)
+        self.UI.pushButton_2.clicked.connect(self, self.setupExcelTable)
 
-    def createAction(self):
-        self.openAction = QAction("Open Excel document")
+    def setupExcelTable(self):
+        """
+        Function to open a filedialog. Does not limit selection to .xls files as of now.
+        :return: Path to a file.
+        """
+        filePath = filedialog.askopenfilename()
+        if filePath:
+            document = parseDocument(filePath)
+
 
 
 app = QApplication(sys.argv)
